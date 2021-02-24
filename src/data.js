f161b2f0627e9d4341795dcef6ca27c7be93507a
gabrielaguerrac <gguerracast@gmail.com>
2021-02-24 17:41:49 -0500

    Object.values como input en todas las funciones

--- src/data.js
+++ src/data.js
@@ -1,56 +1,17 @@
-// // estas funciones son de ejemplo
+export const filterChampions = (data, condition) => data.filter(propChamp => propChamp.tags.includes(condition));
 
 
-
-
-export const filterChampions = (data, condition) => {
-
-  
-  //console.log(roleSelected);
-
-const filterRoles= data.filter(element => {
-  let propChamp =  element[1];
-  //console.log(propChamp);//si hay {version,...}
-  let rolesChampion= propChamp.tags;
-  //console.log(dos); //los tags 1 o 2 de todos los champions
-  //return console.log('soy el return cuando seleccionas mago');
-
-  let filteredByTags = rolesChampion.includes(condition);
- // console.log(filteredByTags);
-  return filteredByTags;
-   //console.log(dos); //los tags 1 o 2 de todos los champions
-  //return console.log('soy el return cuando seleccionas mago');
-  //  let conCondition = (algo == 'true') ?  console.log('true') : console.log('false');
-  // return console.log(conCondition);//devuelve undefined
-
-});
-return filterRoles;
-
-}
-  
-
-
-export const computeStats = (data) => {
-  
-  const result = data.sort((a,b)=>{
-   // return {b-a};
-  })
-  return Object.entries(data) ;
-
-}
-
-
-export const orderChampions = (data, sortOrder) => {
-  //console.log(data)
+export const orderChampions = (data, sortBy, sortOrder) => {
+  console.log(data)
   if ( sortOrder == "aZ"){
     
     const result = data.sort((a,b)=>{
-        // console.log(a.name);
-        // console.log(b.name);
-      if (b > a){
+         //console.log(a[sortBy]);
+         //console.log(b[sortBy]);
+      if (b[sortBy] > a[sortBy]){
         return -1;
     }
-      if (b < a){
+      if (b[sortBy] < a[sortBy]){
         return 1;
     }
 
@@ -63,12 +24,11 @@ export const orderChampions = (data, sortOrder) => {
   } else {
 
     const result = data.sort((a,b)=>{
-      // console.log(a.name);
-      // console.log(b.name);
-    if (b > a){
+      
+    if (b[sortBy] > a[sortBy]){
       return 1;
   }
-    if (b < a){
+    if (b[sortBy] < a[sortBy]){
       return -1;
   }
 
@@ -81,12 +41,12 @@ export const orderChampions = (data, sortOrder) => {
   
 }
 
-// export const filterRoles = (data, condition) => {
+export const computeStats = (data) => {
+  
+  const result = data.sort((a,b)=>{
+   // return {b-a};
+  })
+  return Object.entries(data) ;
 
-//   Object.entries(data.data).filter(element => {
-//   let propChamp =  element[1];
-//   console.log(propChamp);
-//  return propChamp.tags[0].includes(condition);
-// });
-// //showChampions(filter);
-// }
+}
+
